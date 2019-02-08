9df32e1672838f56b21155a97245eea3da8ffc87
VikVik02 <vikstar21997@mail.ru>
2019-02-08 21:49:41 +1000

    Сделала дз уровней easy и normal

--- Lesson_5/hw05_easy.py
+++ Lesson_5/hw05_easy.py
@@ -2,13 +2,47 @@
 # Напишите скрипт, создающий директории dir_1 - dir_9 в папке,
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
+import os
 
+def make_directory():
+    for i in range(1, 10)
+        dir_path = os.path.join(os.getcwd(), 'dir_{}'.format(i))
+        try:
+            os.mkdir(dir_path)
+        except FileExistsError:
+            print("Директория с данным именем уже есть в папке")
+
+def delete_directory():
+    for i in range(1, 10)
+        dir_path = os.path.join(os.getcwd(), 'dir_{}'.format(i))
+        try:
+            os.rmdir(dir_path)
+        except FileNotFoundError:
+            print("Директории с данным именем нет в папке")
+
+make_directory()
+delete_directory()
 
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
 #ТОЛЬКО ПАПКИ, А НЕ ФАЙЛЫ!
 
+dir_list = os.listdir()
+only_dir = []
+for d in dir_list:
+    if os.path.isdir(d):
+        only_dir.append(d)
+print(only_dir)
+
 
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
 # ИСПОЛЬЗОВАТЬ ТОЛЬКО МОДУЛЬ OS
+import shutil
+
+def duplicate_file():
+    base_name = os.path.basename(__file__)
+    file_path = os.path.join(os.getcwd(), 'copy_{}'.format(base_name))
+    shutil.copyfile(__file__, file_path)
+
+duplicate_file()
--- Lesson_5/hw05_normal.py
+++ Lesson_5/hw05_normal.py
@@ -14,7 +14,41 @@
 # оформленные в виде соответствующих функций,
 # и импортированные в данный файл из easy.py
 # ИСПОЛЬЗОВАТЬ МОДУЛЬ OS и SHUTIL
+import shutil, os, sys, hw05_easy
 
+def do_1():
+    dir_name = input("Укажите название папки")
+    dir_path = os.path.join(os.getcwd(), dir_name)
+    try:
+        os.chdir(dir_path)
+    except Exception:
+        print('Такой папки не существует')
+
+def do_2():
+    print("Содержимое данной папки: ", hw05_easy.listdir())
+
+def do_3():
+    dir_name = input("Укажите название папки")
+    dir_path = os.path.join(os.getcwd(), dir_name)
+    shutil.rmtree(dir_path)
+
+def do_4():
+    dir_name = input("Укажите название папки")
+    dir_path = os.path.join(os.getcwd(), dir_name)
+    os.mkdir(dir_path)
+
+
+command = 'start'
+while command != '':
+    command = input("Выберите действие: 1-перейти в папку, 2-просмотреть папку, 3-удалить папку, 4-создать папку")
+    if command == str(1):
+        do_1()
+    elif command == str(2):
+        do_2()
+    elif command == str(3):
+        do_3()
+    elif command == str(4):
+        do_4()
 
 
 
